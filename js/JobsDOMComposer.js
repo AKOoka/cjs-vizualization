b5b251f4e882ca822bdf8fc576ffe2a90a0a3901
AKOoka <gubinovdmitrij@gmail.com>
2020-06-28 23:30:57 +0300

    remade generteColorsByModel

--- js/JobsDOMComposer.js
+++ js/JobsDOMComposer.js
@@ -1,27 +1,92 @@
+function convertHSVToRGB (hue, saturation, value) {
+  const h = hue / 360
+  const s = saturation / 100
+  const v = value / 100
+  const i = Math.floor(h * 6)
+  const f = h * 6 - i
+  const p = v * (1 - s)
+  const q = v * (1 - f * s)
+  const t = v * (1 - (1 - f) * s)
+
+  let r = 0
+  let g = 0
+  let b = 0
+
+  switch (i % 6) {
+    case 0:
+      r = v
+      g = t
+      b = p
+
+      break
+    case 1:
+      r = q
+      g = v
+      b = p
+
+      break
+    case 2:
+      r = p
+      g = v
+      b = t
+
+      break
+    case 3:
+      r = p
+      g = q
+      b = v
+
+      break
+    case 4:
+      r = t
+      g = p
+      b = v
+
+      break
+    case 5:
+      r = v
+      g = p
+      b = q
+
+      break
+  }
+
+  return `${Math.round(r * 255)}, ${Math.round(g * 255)}, ${Math.round(b * 255)}`
+}
+
 class JobsDOMComposer {
   constructor () {
     this.jobsDOMModel = null
   }
 
   generateColorsByModel (model) {
+    const jobNames = new Set()
     const jobColors = new Map()
-    const hStep = 360 / model.jobRecords.size
-    const sStep = 75 / model.jobRecords.size
-    const lStep = 50 / model.jobRecords.size
 
-    let i = 0
+    for (const { name } of model.jobRecords.values()) {
+      jobNames.add(name)
+    }
 
-    const jobs = [...model.jobRecords.keys()]
+    const sortedJobNames = [...jobNames.values()]
+    const hueStep = 120 / sortedJobNames.length * 4
+    let saturation = 60
+    let value = 75
 
-    while (jobs.length > 0) {
-      const jobIndex = Math.floor(Math.random() * jobs.length)
-      const jobId = jobs[jobIndex]
+    for (let i = 0; i < sortedJobNames.length; i++) {
+      const hue = i * hueStep
 
-      jobColors.set(jobId, `${hStep * i}, ${sStep * i + 25}%, ${lStep * i + 25}%`)
+      if (hue > 360) {
+        if (saturation === 60) {
+          saturation = 100
+        } else if (value === 75) {
+          value = 100
+        } else {
+          value = 100
+          saturation = 60
+        }
+      }
 
-      jobs.splice(jobIndex, 1)
-
-      i++
+      jobColors.set(sortedJobNames[i], convertHSVToRGB(hue, saturation, value))
     }
 
     return jobColors
@@ -49,7 +114,7 @@ class JobsDOMComposer {
       rangeDiv.title = name
       rangeDiv.style.left = `${beginTimestamp}px`
       rangeDiv.style.width = `${rangeWidth}px`
-      rangeDiv.style.backgroundColor = `hsl(${jobColors.get(job)})`
+      rangeDiv.style.backgroundColor = `rgb(${jobColors.get(name)})`
 
       if (rangeWidth <= 40) {
         rangeDiv.classList.add('hiddeText')
